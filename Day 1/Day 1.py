cee20b5460f3f7543286c5d5cc2f237b0cc54a56
MuazAlhaidar <omunam10@gmail.com>
2020-12-02 08:01:21 -0500

    Completed Day 1 Part 2

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -219,4 +219,18 @@ def multiplyTwoSum(nums, arr):
     return nums[arr[0]] * nums[arr[1]]
 
 
+def threeSum(nums, target):
+    required = {}
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if (target - (nums[i] + nums[j])) in required:
+                return [required[target - (nums[i] + nums[j])], i, j]
+        required[nums[i]] = i
+
+
+def multiplyThreeSum(nums, arr):
+    return nums[arr[0]] * nums[arr[1]] * nums[arr[2]]
+
+
 print(multiplyTwoSum(numberSet, twoSum(numberSet, target)))
+print(multiplyThreeSum(numberSet, threeSum(numberSet, target)))
